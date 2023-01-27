d7e092398e5db361b4ea39eebb05b8478dec4e58
Cle1cy <psydiego1@gmail.com>
2023-01-26 19:44:08 -0500

    JS

---
+++ js/scrip.js
@@ -0,0 +1,21 @@
+let home = document.querySelector('#home');
+let p1 = document.querySelector('#p1');
+let p2 = document.querySelector('#p2');
+let p3 = document.querySelector('#p3');
+let p4 = document.querySelector('#p4');
+
+function color(){   
+    
+        p1.classList.add('colorh');
+ }
+
+p1.addEventListener('click', color);
+
+
+
+
+
+
+
+
+
